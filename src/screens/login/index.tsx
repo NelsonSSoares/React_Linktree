91b840421fa69e65a970ce47ba401eeeb9dd18de
Nelson <punkpunkada@gmail.com>
2026-01-29 17:20:19 -0300

    login page

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,6 +1,20 @@
+import { useState, type FormEvent } from "react";
 import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent): void {
+    e.preventDefault();
+    console.log({
+      email: email,
+      password: password
+    });
+    
+  }
+
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
       <Link to="/">
@@ -11,6 +25,27 @@ export function Login() {
           </span>
         </h1>
       </Link>
+      <form onSubmit={handleSubmit}
+        className="w-full max-w-xl flex flex-col px-1">
+        <Input
+          type="email"
+          placeholder="Type your e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Type your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 text-white border-0 text-lg font-medium rounded-md"
+        >
+          Access
+        </button>
+      </form>
     </div>
   );
 }
