95706e01742f84d9676de75579373f2d18bae20c
Nelson <punkpunkada@gmail.com>
2026-01-29 12:24:50 -0300

    logo fix

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 
 export function Login() {
-    return (
-        <div className="flex flex-col w-full h-screen items-center justify-center">
-            <Link to="/">
-            <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev</h1>
-            <span className="bg-linear-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full h-screen items-center justify-center">
+      <Link to="/">
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Dev
+          <span className="bg-linear-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+            Link
+          </span>
+        </h1>
+      </Link>
+    </div>
+  );
 }
