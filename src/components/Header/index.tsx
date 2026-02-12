37d82a2bbc23828540e3d511b860d8b2cd1681b2
Nelson <punkpunkada@gmail.com>
2026-02-12 10:30:33 -0300

    social medias

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,7 +15,7 @@ export function Header() {
                 <div className="flex gap-4 font-medium">
                     <Link to="/">Home</Link>
                     <Link to="/admin">Links</Link>
-                    <Link to="/admin/social">Redes Sociais</Link>
+                    <Link to="/admin/networks">Redes Sociais</Link>
                 </div>
                 <button onClick={handleLogOut}>
                     <BiLogOut size={28} color="#db2629" />
--- src/screens/networks/index.tsx
+++ src/screens/networks/index.tsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import {db } from "../../services/firebaseConnection";
+import { setDoc, doc } from "firebase/firestore";
+import { Input } from "../../components/Input";
+
 export function Networks() {
-    return (
-        <div>
-            <h1>NETWORKS</h1>
-        </div>
-    )
+  const [facebookLink, setFacebookLink] = useState("");
+  const [instagramLink, setInstagramLink] = useState("");
+  const [linkedinLink, setLinkedinLink] = useState("");
+  const [youtubeLink, setYoutubeLink] = useState("");
+
+  function handleRegister(e: React.FormEvent) {
+    e.preventDefault();
+    setDoc(doc(db, "social", "link"), {
+        facebook: facebookLink,
+        instagram: instagramLink,
+        linkedin: linkedinLink,
+        youtube: youtubeLink
+    })
+    .then(() => {
+        alert("Link saved successfully!");
+
+    }).catch((error) => {
+        alert("Error saving link: ", error);
+    });
+  }
+
+  return (
+    <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+      <Header />
+      <h1 className="text-white text-2xl font-medium mt-8 mb-4">My Networks</h1>
+      <form className="flex flex-col max-w-xl w-full" onSubmit={handleRegister}>
+        <label className="text-white font-medium mt-2 mb-2">
+          Facebook Link
+        </label>
+        <Input
+          type="url"
+          placeholder="Type your Facebook url.."
+          value={facebookLink}
+          onChange={(e) => setFacebookLink(e.target.value)}
+        />
+
+        <label className="text-white font-medium mt-2 mb-2">
+          Instagram Link
+        </label>
+        <Input
+          type="url"
+          placeholder="Type your Instagram url.."
+          value={instagramLink}
+          onChange={(e) => setInstagramLink(e.target.value)}
+        />
+
+        <label className="text-white font-medium mt-2 mb-2">
+          LinkedIn Link
+        </label>
+        <Input
+          type="url"
+          placeholder="Type your LinkedIn url.."
+          value={linkedinLink}
+          onChange={(e) => setLinkedinLink(e.target.value)}
+        />
+
+        <label className="text-white font-medium mt-2 mb-2">YouTube Link</label>
+        <Input
+          type="url"
+          placeholder="Type your YouTube url.."
+          value={youtubeLink}
+          onChange={(e) => setYoutubeLink(e.target.value)}
+        />
+
+        <button type="submit" className="text-white bg-blue-600 h-9 rounded-md items-center justify-center flex mb-7 font-medium">
+            Save Link
+        </button>
+      </form>
+    </div>
+  );
 }
